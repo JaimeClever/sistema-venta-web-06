3f9799703a5dfd9875e73ba87b8b86fc13487455
JaimeClever <jaimecastillo.140920@gmail.com>
2025-06-19 13:39:29 -0500

    Cambios de constructores en entity Usuario

--- src/main/java/com/sistema_venta_06/grupo06/entity/Usuario.java
+++ src/main/java/com/sistema_venta_06/grupo06/entity/Usuario.java
@@ -1,9 +1,7 @@
 package com.sistema_venta_06.grupo06.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,6 +12,8 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
 public class Usuario {
 
 
@@ -31,4 +31,62 @@ public class Usuario {
             inverseJoinColumns = @JoinColumn(name = "rol_id"))
     private Set<Rol> roles = new HashSet<>();
 
+
+    public Usuario(String username, String password, String nombres) {
+        this.username = username;
+        this.password = password;
+        this.nombres = nombres;
+    }
+    public Usuario(Long id, String username, String password, String nombres) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.nombres = nombres;
+    }
+    public Usuario(String username, String password, String nombres, Set<Rol> roles) {
+        this.username = username;
+        this.password = password;
+        this.nombres = nombres;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
 }
