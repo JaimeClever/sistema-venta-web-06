d25cebbb62a6a5fee2cbe0411d4fcda84ce3c120
JaimeClever <jaimecastillo.140920@gmail.com>
2025-06-19 16:42:59 -0500

    creacion del metodo para validar usuarios

--- src/main/java/com/sistema_venta_06/grupo06/service/UsuarioService.java
+++ src/main/java/com/sistema_venta_06/grupo06/service/UsuarioService.java
@@ -78,4 +78,16 @@ public class UsuarioService {
     }
 
 
+    //Implemntacion del metodo para verificar usuarios
+    public Usuario loginUsuario(String username, String password) {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        if (!passwordEncoder.matches(password, usuario.getPassword())) {
+            throw new RuntimeException("Contraseña incorrecta");
+        }
+
+        return usuario;
+    }
+
 }
