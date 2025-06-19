bc10b5c95627710765c111b71e387c248014a04a
JaimeClever <jaimecastillo.140920@gmail.com>
2025-06-18 19:14:15 -0500

    Primer commit del proyecto

---
+++ src/test/java/com/sistema_venta_06/grupo06/Grupo06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_venta_06.grupo06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
