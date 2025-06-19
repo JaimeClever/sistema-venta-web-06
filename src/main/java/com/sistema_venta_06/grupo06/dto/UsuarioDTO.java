77af7abe3d95113cca7541fe869a9748322873ef
JaimeClever <jaimecastillo.140920@gmail.com>
2025-06-19 14:58:28 -0500

    cambios en getter y setter de usuarioDTO

--- src/main/java/com/sistema_venta_06/grupo06/dto/UsuarioDTO.java
+++ src/main/java/com/sistema_venta_06/grupo06/dto/UsuarioDTO.java
@@ -4,11 +4,41 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class UsuarioDTO {
     private String username;
     private String password;
     private String nombres;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(String username, String password, String nombres) {
+        this.username = username;
+        this.password = password;
+        this.nombres = nombres;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
 }
